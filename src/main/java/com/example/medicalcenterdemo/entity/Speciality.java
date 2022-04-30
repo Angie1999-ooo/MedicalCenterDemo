9593c93807459787d2604ddb4828b175099e7835
Angie1999-ooo <69354433+Angie1999-ooo@users.noreply.github.com>
2022-04-30 13:28:04 +0300

    Add files via upload

---
+++ src/main/java/com/example/medicalcenterdemo/entity/Speciality.java
@@ -0,0 +1,46 @@
+package com.example.medicalcenterdemo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.hibernate.annotations.Where;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Data
+@EnableAutoConfiguration
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="speciality")
+public class Speciality implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String speciality;
+
+    public Speciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Speciality that = (Speciality) o;
+        return Objects.equals(id, that.id) && Objects.equals(speciality, that.speciality);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, speciality);
+    }
+}
