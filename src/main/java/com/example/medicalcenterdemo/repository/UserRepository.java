dbb98a8859ac76d25e81989843d9337905c1e193
Angie1999-ooo <69354433+Angie1999-ooo@users.noreply.github.com>
2022-05-01 18:41:55 +0300

    Add files via upload

---
+++ src/main/java/com/example/medicalcenterdemo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.medicalcenterdemo.repository;
+
+
+import com.example.medicalcenterdemo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
