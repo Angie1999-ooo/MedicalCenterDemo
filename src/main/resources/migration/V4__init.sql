668798ba4b4ec94c5e57c5b547f93bbb2769d544
Angie1999-ooo <69354433+Angie1999-ooo@users.noreply.github.com>
2022-05-11 14:34:46 +0300

    Delete V4__init.sql

--- src/main/resources/migration/V4__init.sql
+++
@@ -1,9 +0,0 @@
-create table patient (
-                            id bigserial primary key,
-                            first_name character varying(255),
-                            middle_name character varying(255),
-                            second_name character varying(255),
-                            phone character varying(255),
-                            email character varying(255)
-
-)
